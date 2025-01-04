12bb7f926a182e3f591a0f5af8a691bfe2bdb6d7
Mahindra Roshan M <mahindraroshan413@gmail.com>
2025-01-04 11:00:57 +0530

    2 pointer+ hash

---
+++ Strings/MEDIUM/1930. Unique Length-3 Palindromic Subsequences.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        res = set()
+        left = set()
+        right = Counter(s)
+
+        for mid in range(len(s)):
+            right[s[mid]]-=1
+            if right[s[mid]] == 0:
+                right.pop(s[mid])
+            for c in left:
+                if c in right:
+                    res.add(c+s[mid]+c)
+        
+            left.add(s[mid])
+        return len(res)
